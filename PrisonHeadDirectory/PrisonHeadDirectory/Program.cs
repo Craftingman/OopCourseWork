d6ddd49c51c1575b5382fc7883e945a36ea711f5
Victor Zherebniy <craftingman111@gmail.com>
2021-05-14 14:24:38 +0300

    Change Solution Structure

--- PrisonHeadDirectory/PrisonHeadDirectory/Program.cs
+++ PrisonHeadDirectory/PrisonHeadDirectory/Program.cs
@@ -18,6 +18,9 @@ namespace PrisonHeadDirectory
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
